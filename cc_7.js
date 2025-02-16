e0f71092f26ae6c3b7d99a7aeb3e943413a96196
Ramy <ramy3@usf.edu>
2025-02-16 15:12:14 -0500

    Task 6 - Filtering High-Value Transactions.

--- cc_7.js
+++ cc_7.js
@@ -52,3 +52,12 @@ function calculateLoanInterest(principal, rate, years) { // calculate and make a
 }
 calculateLoanInterest(1000, 0.05, 3); // Expected output: "Total Interest: $150.00"
 calculateLoanInterest(5000, 0.07, 5); // Expected output: "Total Interest: $1750.00"
+
+
+// Task 6 - HIgh-Order Function
+let transactions = [500, 1200, 3000, 800, 2200];
+const filterHighValueTransactions = (transactions, filterFunction) => {
+    return transactions.filter(filterFunction);
+};
+console.log(filterHighValueTransactions(transactions, amount => amount > 1000));
+
